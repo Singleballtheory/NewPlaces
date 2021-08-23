4c5ff1fcc0288c20dd2540219e8f7a3cf377dedf
Scott Hutley <scotthutley1@comcast.net>
2021-08-23 13:53:09 -0700

    Create assignID and findLocation functions
    Co-authored-by: Melissa Schatz-Miller <melissa.scmi@gmail.com>

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ Test: "It should display Location info when location is entered"
 Code: testLocation;
 Expected Output: Location {countryName: "US", landmark: "Welcome to Portland sign", timeOfYear: "Fall", notes: "here is a note"}
 
+Test: "It should disply all locations information when you access travelGuide locations"
+Code: travelGuide.locations;
+Expected Output: US: 
+Canada: Location {countryName: "Canada", landmark: "The Butchart Gardens", timeOfYear: "Summer", notes: "Fountains and flowers."}
+Mexico: Location {countryName: "Mexico", landmark: "Castillo Chapultepec", timeOfYear: "Spring", notes: "That's a lot of strained glass!"}
+UK: Location {countryName: "UK", landmark: "Edinburgh", timeOfYear: "Winter", notes: "Closes and wynds."}
+US: Location {countryName: "US", landmark: "Welcome to Portland sign", timeOfYear: "Fall", notes: "We live here; it's nice."}
+
+Test: "It should display the information for the location at the id entered."
+Code: travelGuide.findLocation(4);
+Expected Output: Location {countryName: "UK", landmark: "Edinburgh", timeOfYear: "Winter", notes: "Closes and wynds.", id: 4}
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
 
+function TravelGuide() {
+  this.locations = {};
+  this.currentId = 0;
+}
+
+TravelGuide.prototype.addLocation = function(location) {
+  location.id = this.assignId();
+  this.locations[location.id] = location;
+};
+
+TravelGuide.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+TravelGuide.prototype.findLocation = function(id) {
+  if (this.locations[id] != undefined) {
+    return this.locations[id];
+  }
+  return false;
+};
+
 function Location (countryName, landmark, timeOfYear, notes) {
   this.countryName = countryName;
   this.landmark = landmark;
@@ -6,12 +28,37 @@ function Location (countryName, landmark, timeOfYear, notes) {
   this.notes = notes;
 }
 
-let testLocation = new Location("US", "Welcome to Portland sign", "Fall", "here is a note");
+let travelGuide = new TravelGuide();
+let newLocation = new Location("US", "Welcome to Portland sign", "Fall", "We live here; it's nice.");
+let newLocation2 = new Location("Mexico", "Castillo Chapultepec", "Spring", "That's a lot of strained glass!");
+let newLocation3 = new Location("Canada", "The Butchart Gardens", "Summer", "Fountains and flowers.")
+let newLocation4 = new Location('UK', "Edinburgh", "Winter", "Closes and wynds.")
+travelGuide.addLocation(newLocation);
+travelGuide.addLocation(newLocation2);
+travelGuide.addLocation(newLocation3);
+travelGuide.addLocation(newLocation4);
 
 
 
+// function AddressBook() {
+//   this.contacts = {};
+// }
 
+// AddressBook.prototype.addContact = function(contact) {
+//   this.contacts[contact.firstName] = contact;
+// };
 
+// function Contact(firstName, lastName, phoneNumber) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.phoneNumber = phoneNumber;
+// }
+
+// let addressBook = new AddressBook();
+// let contact = new Contact("Ada", "Lovelace", "503-555-0100");
+// let contact2 = new Contact("Grace", "Hopper", "503-555-0199");
+// addressBook.addContact(contact);
+// addressBook.addContact(contact2);
 // let us = {name: "United States of America"};
 // let mx = {name: "Mexico"};
 // let cn = {name: "Canada"};
